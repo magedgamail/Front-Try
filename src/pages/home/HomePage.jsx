1600361d6e94c9b7c01dc7fb560e806c1951ecba
MagedGamail <magedgamail2391@gmail.com>
2025-12-06 13:00:06 +0200

    first commit

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,42 @@
+
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router';
+import { Header } from '../../components/Header.jsx';
+import { ProductGrid } from './ProductGrid.jsx';
+import './HomePage.css';
+
+export function HomePage({ cart, loadCart }) {
+    const [products, setProducts] = useState ([]);
+    const [searchParams] = useSearchParams();
+    const search = searchParams.get('search');
+    
+
+    useEffect(() => {
+        const getHomeData = async () => {
+
+            const urlPath = search ? `/api/products?search=${search}` : '/api/products';
+            const response = await axios.get(urlPath);
+            setProducts(response.data);
+        } 
+            
+
+        getHomeData();
+    }, [search]);
+
+
+
+    return (
+        <>
+            <title>Ecommerce project</title>
+
+            <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
+
+            <Header cart={cart} /> 
+
+            <div className="home-page">
+                <ProductGrid products={products} loadCart={loadCart} />
+            </div>
+        </>
+    )
+}
